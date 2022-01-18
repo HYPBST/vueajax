bf17cccb7f3674e4529812e1850d6da457d3a57d
Schmidt Gergő <s.gergo.az@gmail.com>
2022-01-18 16:48:50 +0100

    komponensekbe rendezés

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,44 @@
 <template>
-  <div id="app">
-    
-    <table>
-      <thead>
-        <tr>
-          <th>Azonsító</th>
-          <th>Alkotó</th>
-          <th>Magasság</th>
-          <th>Ár</th>
-          <th>Műveletek</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="statue in statues" v-bind:key="statue.id">
-          <td>{{ statue.id }}</td>
-          <td>{{ statue.person }}</td>
-          <td>{{ statue.height }}</td>
-          <td>{{ statue.price }}</td>
-          <td>
-            <button @click="deletestatue(statue.id)">Törlés</button>
-            <button @click="editstatue(statue.id)">Szerkesztés</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="hidden" v-model="statue.id">
-          </td>
-          <td>
-            <input type="text" v-model="statue.person">
-          </td>
-          <td>
-            <input type="number" v-model="statue.height">
-          </td>
-          <td>
-            <input type="number" v-model="statue.price">
-          </td>
-          <td>
-            <button v-if="mod_new" @click="newstatue" :disabled="saving">Létrehoz</button>
-            <button v-if="!mod_new" @click="savestatue" :disabled="saving">Mentés</button>
-            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving">Mégse</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <button @click="loadData">Adatok betöltése</button>
-  </div>
+<div id="app">
+  <button @click="festmeny">Festmények</button>
+  <button @click="szobrok">Szobrok</button>
+  <Painting v-if="festmeny_on"/>
+  <Statue v-if="!festmeny_on"/>
+
+</div>
+  
 </template>
 
 <script>
-
+import Painting from './components/Painting.vue'
+import Statue from './components/Statue.vue'
 export default {
+
+  
   name: 'App',
   components: {
+    Painting,
+    Statue
   },
   data() {
     return {
-      mod_new: true, 
-      saving: false,
-      statue: {
-        id: null,
-        person: '',
-        height: '',
-        price: ''
-      },
-      statues: []
+      festmeny_on: true,
     }
   },
   methods: {
-    async loadData () {
-     let Response = await fetch('http://127.0.0.1:8000/api/statues')
-     let data = await Response.json()
-     this.statues = data
-    },
-    async deletestatue(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
-        method: 'DELETE'
-      })
-      console.log(Response)
-      await this.loadData()
-    },
-    async newstatue() {
-      this.saving='disabled'
-     await fetch('http://127.0.0.1:8000/api/statues', {
-       method: 'POST',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async savestatue() {
-      this.saving='disabled'
-     await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`, {
-       method: 'PATCH',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.statue) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async editstatue(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
-      let data = await Response.json()
-      this.statue = {...data};
-      this.mod_new = false
-    },
-    cancelEdit () {
-      this.resetForm()
-    },
-    resetForm() {
-      this.statue = {
-        id: null,
-        person: '',
-        height: '',
-        price: ''
+    festmeny(){
+      if(this.festmeny_on!=true) {
+        this.festmeny_on = true;
       }
-      this.mod_new = true
+    },
+    szobrok(){
+      if(this.festmeny_on==true) {
+        this.festmeny_on = false;
+      }
     }
-  },
-  mounted() {
-    this.loadData()
-  }
+    }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
